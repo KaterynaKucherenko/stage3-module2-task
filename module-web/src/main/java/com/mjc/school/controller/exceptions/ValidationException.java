9a4793f0ee0e25271c31da0b55bcaa4c771abc2e
KaterynaKucherenko <estorskaya@gmail.com>
2023-09-27 15:55:02 +0300

    Revert "Revert "Revert "save1"""
    This reverts commit a5ebe577bb9dac33f1a242a2c27b41c4f36c519c.

--- module-web/src/main/java/com/mjc/school/controller/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.mjc.school.controller.exceptions;
-
-public class ValidationException extends RuntimeException{
-    public ValidationException(String message){
-        super(message);
-    }
-}
